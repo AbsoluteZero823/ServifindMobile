5f75e0cc3fbf23484fcac1292c10c36554ff3ecc
PoorKit <zekiriel2000@gmail.com>
2023-05-23 19:02:34 +0800

    changed it to request_id

--- src/models/offer.js
+++ src/models/offer.js
@@ -8,7 +8,7 @@ export const Offer = types.model('Offer', {
   created_At: types.optional(types.Date, () => new Date()),
   offer_status: types.optional(types.enumeration(['waiting', 'cancelled', 'granted']), 'waiting'),
   offered_by: types.reference(types.late(() => User)),
-  request_id: types.reference(types.late(() => Request)),
+  inquiry_id: types.reference(types.late(() => Inquiry)),
 }).actions((self) => ({
     setoffer_status(status) {
         self.offer_status = status;
